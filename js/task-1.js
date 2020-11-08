2f098e12871ec558e37e1ce8a86ad531fa0745c2
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-08 17:04:40 +0200

    добавляет частичное решение задачи

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,10 @@
+// const quantityOfCategoryItem = document.querySelector('#categories').children.length;
 
+const quantityOfCategoryItem = document.querySelector('ul#categories').childElementCount;
+
+console.log(`В списке ${quantityOfCategoryItem} категории`)
+
+// const categoryItem = document.querySelectorAll('h2');
+
+
+// console.log(categoryItem)
