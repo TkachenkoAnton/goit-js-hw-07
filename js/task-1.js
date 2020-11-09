9c9e7f350499273347c1560fbc8402006bba00f5
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-09 22:38:57 +0200

    добавляет частичное решение задачи 1

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,9 @@ const category = document.querySelector('#categories');
 
 console.log(`В списке ${category.childElementCount} категории`)
 
-// const categoryItem = document.querySelectorAll('h2');
+const categoryItemTitle = document.querySelectorAll('h2');
+
+const getItemTitle = [...categoryItemTitle].map(({ textContent }) => [`Категория: ${textContent}`]);
 
 
-// console.log(categoryItem)
+console.log(getItemTitle)
