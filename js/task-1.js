8851cf6409abd2ff553727880e3cfa9aec7e242a
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-11 21:06:57 +0200

    добавляет альтернативное решение

--- js/task-1.js
+++ js/task-1.js
@@ -9,3 +9,6 @@ const categoryItem = document.querySelectorAll('.item');
 const getItem = categoryItem.forEach(item =>
 console.log(`Категория: ${item.children[0].textContent}\nКоличество элементов: ${item.children[1].childElementCount}`));
 
+// const getItem = categoryItem.forEach(item =>
+// console.log(`Категория: ${item.firstElementChild.textContent}\nКоличество элементов: ${item.lastElementChild.childElementCount}`));
+
