9f29471d8f7fafe2b59339983280487110189c77
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-12 19:37:58 +0200

    добавляет решение 6 задачи

---
+++ js/task-6.js
@@ -0,0 +1,24 @@
+const inputRef = document.querySelector('#validation-input')
+
+const dataLength = Number(inputRef.dataset.length);
+
+inputRef.addEventListener('blur', () => {
+    
+    if (inputRef.value.length === dataLength) {
+
+        inputRef.classList.add('valid');
+        inputRef.classList.replace('invalid', 'valid');
+
+    } else if (inputRef.value.length > 0 && inputRef.value.length < dataLength
+        || inputRef.value.length > dataLength) { 
+        
+        inputRef.classList.add('invalid');
+        inputRef.classList.replace('valid', 'invalid');
+
+    } else if (inputRef.value.length === 0) {
+
+        inputRef.classList.remove('valid');
+        inputRef.classList.remove('invalid');
+
+    }
+})
