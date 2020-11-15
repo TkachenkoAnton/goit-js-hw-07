8893e0862d0cdd6501c0007de8b7e81944771533
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-15 14:00:22 +0200

    добавляет частичное решение

--- js/task-8.js
+++ js/task-8.js
@@ -11,12 +11,23 @@ let inputCurrentValue = '';
 const getCurrentValue = () => inputCurrentValue = inputRef.valueAsNumber;
 
 const createBoxes = (amount) => {
-
+    
     for (let i = 0; i < amount; i += 1) {
-        const divCreate = document.createElement('div');
-        divCreate.append(i + 1);
-        boxesRef.append(divCreate);    
+        boxesRef.insertAdjacentHTML('beforeend', `<div>${i + 1}</div>`);
+
+        // const divStyleAll = document.querySelector('#boxes').children;
+        //     divStyleAll[0].style.backgroundColor = 'grey';
+        //     divStyleAll[0].style.width = `30px`;
+        //     divStyleAll[0].style.height = `30px`;
+
+        // [...divStyleAll].forEach(item => {
+        //     item.style.backgroundColor = 'grey';
+        //     item.style.width = `${divStyleAll[0].clientWidth + 10}px`;
+        //     item.style.height = `30px`;
+        //     console.log(divStyleAll)
+        // });
     };
+
 };
 
 const destroyBoxes = () => {
