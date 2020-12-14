024165e2a948c62944a475a5811a5b6009803d05
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-12-14 19:37:12 +0200

    add method padStart

--- js/task-8.js
+++ js/task-8.js
@@ -2,41 +2,50 @@ const inputRef = document.querySelector('#controls input');
 
 const buttonRenderRef = document.querySelector('button[data-action="render"]');
 
-const buttonDestroyRef = document.querySelector('button[data-action="destroy"]');
+const buttonDestroyRef = document.querySelector(
+  'button[data-action="destroy"]',
+);
 
 const boxesRef = document.querySelector('#boxes');
 
 let inputCurrentValue = '';
 
-const getCurrentValue = () => inputCurrentValue = inputRef.valueAsNumber;
+const getCurrentValue = () => (inputCurrentValue = inputRef.valueAsNumber);
 
 const randColor = () => {
-    const r = Math.floor(Math.random() * (256));
-    const g = Math.floor(Math.random() * (256));
-    const b = Math.floor(Math.random() * (256));
-    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
-}
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return (
+    '#' +
+    r.toString(16).padStart(2, '0') +
+    g.toString(16).padStart(2, '0') +
+    b.toString(16).padStart(2, '0')
+  );
+};
 
-const createBoxes = (amount) => {
-    
-    for (let i = 0; i < amount; i += 1) {
-        boxesRef.insertAdjacentHTML('beforeend', `<div>${i + 1}</div>`);
-    };
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i += 1) {
+    boxesRef.insertAdjacentHTML('beforeend', `<div>${i + 1}</div>`);
+  }
 
-    const divStyleAll = document.querySelectorAll('#boxes div');
+  const divStyleAll = document.querySelectorAll('#boxes div');
 
-    divStyleAll.forEach((item, i) => {
-        item.style.backgroundColor = randColor();
-        item.style.width = `${30 + i * 10}px`;
-        item.style.height = `${30 + i * 10}px`;
-    });
+  divStyleAll.forEach((item, i) => {
+    item.style.backgroundColor = randColor();
+    item.style.width = `${30 + i * 10}px`;
+    item.style.height = `${30 + i * 10}px`;
+  });
 };
 
 const destroyBoxes = () => {
-    const divDestroy = document.querySelectorAll('#boxes div');
-    divDestroy.forEach(item => item.remove());
+  const divDestroy = document.querySelectorAll('#boxes div');
+  divDestroy.forEach(item => item.remove());
 };
 
-buttonRenderRef.addEventListener('click', () => { getCurrentValue(); createBoxes(inputCurrentValue) });
+buttonRenderRef.addEventListener('click', () => {
+  getCurrentValue();
+  createBoxes(inputCurrentValue);
+});
 
 buttonDestroyRef.addEventListener('click', () => destroyBoxes());
